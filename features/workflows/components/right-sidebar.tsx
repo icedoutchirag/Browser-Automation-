2605caa6c4e9b2c4cc48fe005b36981b8abd86dd
icedoutchirag <chiragtayal2006@gmail.com>
2026-08-05 19:40:40 +0530

    Fix JSX closing tag syntax for Delete node button

--- features/workflows/components/right-sidebar.tsx
+++ features/workflows/components/right-sidebar.tsx
@@ -188,8 +188,10 @@ function Inspector({ node }: { node: StepNodeType | undefined }) {
                   <NodeIcon type={connection.nodeType} className="size-4" />
                   <span className="truncate">{connection.label}</span>
                 </button>
-              ))}
             </div>
+          </div>
+        )}
+
         {/* Delete node button */}
         {node.data.kind !== "trigger" && (
           <Button
